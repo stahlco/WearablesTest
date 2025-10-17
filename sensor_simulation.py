724544591bc2ffdbfa95641b63408d9b7c00865c
stahlco <stahl1consti2@gmail.com>
2025-10-17 20:12:07 +0200

    Added ECG-Mock-Sensor

--- sensor_simulation.py
+++ sensor_simulation.py
@@ -5,7 +5,7 @@ import time
 from time import sleep
 
 from paho.mqtt import client as mqttc
-from paho.mqtt.client import CallbackAPIVersion
+from paho.mqtt.client import CallbackAPIVersion, MQTTMessageInfo
 
 HOST = "localhost"
 PORT = 1883
@@ -26,41 +26,70 @@ def simulating_sensor(sensor_type, client_id):
     mqtt_client.connect(HOST, PORT)
     mqtt_client.loop_start()
 
-    topic = ""
-    if sensor_type == "ppg":
-        topic = f"{PPG_PREFIX}{device_id}"
-    else:
-        topic = f"{ECG_PREFIX}{device_id}"
-
     try:
         while not stop_event.is_set():
-            ir_value = random.randint(40000, 60000)
-            red_value = random.randint(40000, 60000)
-            green_value = random.randint(40000, 60000)
-            heart_rate_bpm = random.randint(70, 80)
-            spo2_percent = round(random.uniform(97, 100))
-
-            mock_pl = {
-                "device_id": device_id,
-                "user_id": client_id,
-                "data": [
-                    {
-                        "timestamp": time.strftime("%Y-%m-%d %H:%M:%S"),
-                        "ir_value": ir_value,
-                        "red_value": red_value,
-                        "green_value": green_value,
-                        "heart_rate_bpm": heart_rate_bpm,
-                        "spo2_percent": spo2_percent
-                    }
-                ]
-            }
-            mqtt_client.publish(topic, json.dumps(mock_pl))
+            if sensor_type == "ppg":
+                publish_ppg_message(mqtt_client, device_id, client_id)
+            else:
+                publish_ecg_message(mqtt_client, device_id, client_id)
 
         sleep(SEND_INTERVAL * random.uniform(0, 1))
     finally:
         mqtt_client.loop_stop()
         mqtt_client.disconnect()
         print(f"Device {device_id} stopped")
+
+def publish_ppg_message(mqtt_client, device_id, client_id) -> MQTTMessageInfo:
+    ir_value = random.randint(40000, 60000)
+    red_value = random.randint(40000, 60000)
+    green_value = random.randint(40000, 60000)
+    heart_rate_bpm = random.randint(70, 80)
+    spo2_percent = round(random.uniform(97, 100))
+
+    topic = f"{PPG_PREFIX}{device_id}"
+
+    mock_ppg = {
+        "device_id": device_id,
+        "user_id": client_id,
+        "data": [
+            {
+                "timestamp": time.strftime("%Y-%m-%d %H:%M:%S"),
+                "ir_value": ir_value,
+                "red_value": red_value,
+                "green_value": green_value,
+                "heart_rate_bpm": heart_rate_bpm,
+                "spo2_percent": spo2_percent
+            }
+        ]
+    }
+
+    return mqtt_client.publish(topic, json.dumps(mock_ppg))
+
+
+def publish_ecg_message(mqtt_client, device_id, client_id) -> MQTTMessageInfo:
+    leads = []
+
+    for i in range(1, 6):
+        leads.append(round(random.uniform(-0.2,0.2), 2))
+
+    topic = f"{ECG_PREFIX}{device_id}"
+    heart_rate_bpm = random.randint(70, 100)
+    mock_ecg = {
+        "device_id": device_id,
+        "client_id": client_id,
+        "data": [
+            {
+                "lead-1": leads[1],
+                "lead-2": leads[2],
+                "lead-3": leads[3],
+                "lead-4": leads[4],
+                "lead-5": leads[5],
+                "heart_rate_bpm": heart_rate_bpm
+            }
+        ]
+    }
+
+    return mqtt_client.publish(topic, json.dumps(mock_ecg))
 
 def main():
     ppgs = []
